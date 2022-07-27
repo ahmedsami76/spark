3fef5e7d5647ae13a52411a15c7a9cc41fa2038e
Ahmed Sami <ahmed.samy@gmail.com>
2022-07-27 18:58:38 -0500

    adding notebooks 1

---
+++ spark-hadoop-container/data/SimpleApp.py
@@ -0,0 +1,13 @@
+"""SimpleApp.py"""
+from pyspark.sql import SparkSession
+
+logFile = "file:///usr/local/spark/README.md"  # Should be some file on your system
+spark = SparkSession.builder.appName("SimpleApp").getOrCreate()
+logData = spark.read.text(logFile).cache()
+
+numAs = logData.filter(logData.value.contains('a')).count()
+numBs = logData.filter(logData.value.contains('b')).count()
+
+print("Lines with a: %i, lines with b: %i" % (numAs, numBs))
+
+spark.stop()
